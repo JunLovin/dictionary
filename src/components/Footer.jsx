dc990d54f6d1ba872a12fa9fbf0918229f1c70fe
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-14 11:41:27 -0500

    style(footer): make anchors more lighter when is hovered and disappear when the screen is so small

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
     return (
         <>
-        <footer className="w-[90%] h-[70px] absolute bottom-0 flex justify-between items-center">
-            <div className="footer-left"><a className="text-xl font-bold underline text-neutral-200 underline-offset-2 dark:text-neutral-800 dark:hover:text-purple-500/20" href="https://github.com/JunLovin" target="_blank">GitHub</a></div>
-            <div className="footer-right"><a className="text-xl font-bold underline text-neutral-200 underline-offset-2 dark:text-neutral-800 dark:hover:text-purple-500/20" href="#">Términos</a></div>
+        <footer className="w-[90%] h-[70px] absolute bottom-0 flex justify-between max-sm:hidden items-center">
+            <div className="footer-left"><a className="text-xl font-bold underline text-neutral-200 underline-offset-2 dark:text-neutral-800 dark:hover:text-purple-300/20" href="https://github.com/JunLovin" target="_blank">GitHub</a></div>
+            <div className="footer-right"><a className="text-xl font-bold underline text-neutral-200 underline-offset-2 dark:text-neutral-800 dark:hover:text-purple-300/20" href="#">Terms and conditions</a></div>
         </footer>
         </>
     )
