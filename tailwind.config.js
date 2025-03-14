1381fc4b9f13b2e6b245c041f36b579ffba41889
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-14 09:34:59 -0500

    feat: add poppins and inter options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        "./index.html",
         "./src/**/*.{js,jsx,ts,tsx}",
-        "./public/index.html",
     ],
     darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
+                'poppins': ['Poppins', 'sans-serif'],
+                'inter': ['Inter', 'sans-serif'],
             },
         },
     },
