8fd6ffb15811603b60ed010d3743cf972c7259da
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-14 12:57:10 -0500

    style: change text to yellow when dark mode is off

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -8,7 +8,7 @@ function Error() {
                 <img src={Raccooon} alt="The best gif" className="rounded-full size-80"/>
             </div>
             <div className="flex flex-col gap-4 items-center error-bottom">
-                <h2 className="text-5xl font-extrabold text-center text-black dark:text-purple-500 max-sm:text-4xl">Oops..This word has escaped!</h2>
+                <h2 className="text-5xl font-extrabold text-center text-yellow-500 dark:text-purple-500 max-sm:text-4xl">Oops..This word has escaped!</h2>
                 <p className="text-2xl font-bold text-center text-neutral-400 dark:text-neutral-600">But here's the best gif ever!</p>
             </div>
         </div>
