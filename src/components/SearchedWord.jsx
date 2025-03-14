80c7c5773ec612e74379c95cb3bacfca7a915c40
Mathias Rendon <mathiassaid7@gmail.com>
2025-03-14 11:44:59 -0500

    style: change loading text color

--- src/components/SearchedWord.jsx
+++ src/components/SearchedWord.jsx
@@ -1,29 +1,29 @@
 import { useContext } from 'react'
 import Card from '@components/Card'
 import { UseWordContext } from '@context/UseWord'
+import Raccooon from '@public/raccoon.gif'
 
-function SearchedWord(){
+function SearchedWord() {
     const { data } = useContext(UseWordContext)
 
     if (data === null) {
-        return <div className="py-8 text-center text-neutral-500 dark:text-neutral-300">Loading...</div>
+        return <div className="py-8 text-5xl font-bold text-center text-neutral-500 dark:text-purple-400">Loading...</div>
     }
 
     return (
         <>
-        {data &&   
-            <div className="w-full searched-words">
-                {data.map((item, index) => {
-                    return (
-                        <Card wordInfo={item} description={item.meanings.map((el) => {
-                            return el.definitions.map((el) => {
-                                return el.definition
-                            })
-                        })} id={item.word} audio={item.phonetics[0] && item.phonetics[0].audio ? item.phonetics[0].audio : item.phonetics[1].audio ? item.phonetics[1].audio : item.phonetics[2].audio ? item.phonetics[2].audio : ""} key={index}/>
-                    )
-                })}
-            </div>
-        }
+            {data &&
+                <div className="w-full searched-words">
+                    {data.map((item, index) => {
+                        return (
+                            <Card wordInfo={item} description={item.meanings.map((el) => {
+                                return el.definitions.map((el) => {
+                                    return el.definition
+                                })
+                            })} id={item.word} audio={item.phonetics[0] && item.phonetics[0].audio ? item.phonetics[0].audio : item.phonetics[1].audio ? item.phonetics[1].audio : item.phonetics[2].audio ? item.phonetics[2].audio : ""} key={index} />
+                        )
+                    })}
+                </div>}
         </>
     )
 }
